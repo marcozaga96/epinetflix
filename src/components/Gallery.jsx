f5cb74a42fb0f66401a24ff5352bd36e39548703
Marco Zagaria <marcozagaria@live.com>
2024-08-23 16:27:54 +0200

    aggiunta spinner e messaggio di errore

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { Alert, Col, Container, Row, Spinner } from "react-bootstrap";
 
 class Gallery extends Component {
   componentDidMount = () => {
@@ -6,8 +7,11 @@ class Gallery extends Component {
   };
   state = {
     films: [],
+    isLoading: false,
+    isError: false,
   };
   myFetch = () => {
+    this.setState({ isLoading: true });
     fetch(
       `https://www.omdbapi.com/?apikey=5ace13d8&s=${this.props.singleGallery}`
     )
@@ -19,17 +23,30 @@ class Gallery extends Component {
         }
       })
       .then((arrFilms) => {
-        this.setState({ films: arrFilms.Search.slice(0, 6) });
+        this.setState({ films: arrFilms.Search.slice(0, 6), isLoading: false });
         console.log(arrFilms.Search);
       })
       .catch((err) => {
         console.log(err);
+        this.setState({ isLoading: false, isError: true });
       });
   };
 
   render() {
     return (
       <div>
+        <Container>
+          <Row className="justify-content-center">
+            <Col xs lg="2">
+              {this.state.isLoading && (
+                <Spinner animation="border" variant="info" />
+              )}
+              {this.state.isError && (
+                <Alert variant="danger">Oops! Qualcosa è andato storto</Alert>
+              )}
+            </Col>
+          </Row>
+        </Container>
         <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 ">
           {this.state.films.map((film, i) => (
             <div key={i} className="col mb-2 text-center px-1">
